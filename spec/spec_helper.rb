9bf6895f2193125aa6a2794de0fd4d0ccab1e795
Bee <SmartHoneybee@users.noreply.github.com>
2016-11-21 15:22:23 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
+require 'simplecov'
+require 'coveralls'
 require 'lita-gitlab'
 require 'lita/rspec'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start { add_filter '/spec/' }
 
 def fixture_file(filename)
   return '' if filename == ''
